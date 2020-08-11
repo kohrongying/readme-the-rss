d0784e9623cc3a871009551796a29ef3d62b3f6d
rongying <rongying.koh@thoughtworks.com>
2020-08-11 12:16:05 +0800

    Remove final payload

--- dist/index.js
+++ dist/index.js
@@ -279,8 +279,8 @@ const main = async() => {
 
 
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
   } catch (error) {
     core.setFailed(error.message);
   }
