9e3cdca0a6b401543bde48244659b199e3272a98
rongying <rongying.koh@thoughtworks.com>
2020-08-11 10:57:03 +0800

    test3

--- dist/index.js
+++ dist/index.js
@@ -253,38 +253,41 @@ const getRSSFeed = async (feedURL) => {
   return feed.items
 }
 
-try {
-  const feedURL = core.getInput('feed_url');
-  console.log(`Getting RSS Feed url: ${feedURL}!`);
-  const feed = getRSSFeed(feedURL)
-  console.log(`Got the feed`, feed)
+const main = async() => {
+  try {
+    const feedURL = core.getInput('feed_url');
+    console.log(`Getting RSS Feed url: ${feedURL}!`);
+    const feed = await getRSSFeed(feedURL)
+    console.log(`Got the feed`, feed)
 
-  const mdFeed = formatToMarkdown(feed, 5)
-  console.log(`Formatting feed to md: ${mdFeed}!`);
-  
+    const mdFeed = formatToMarkdown(feed, 5)
+    console.log(`Formatting feed to md: ${mdFeed}!`);
+    
 
-  console.log(`Writing to readme`);
-  replaceMd('README.md', mdFeed)
-  console.log(`Written to readme`);
-
-
-  
-  // 1) use octokit/core to push changes
-//https://github.com/theboi/github-update-readme/blob/master/index.js
-
-  // 2) write changes to readme file then use a push action in next
-//https://medium.com/analytics-vidhya/create-github-actions-and-be-smart-f1e6b9cc9bfa
+    console.log(`Writing to readme`);
+    replaceMd('README.md', mdFeed)
+    console.log(`Written to readme`);
 
 
+    
+    // 1) use octokit/core to push changes
+  //https://github.com/theboi/github-update-readme/blob/master/index.js
 
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+    // 2) write changes to readme file then use a push action in next
+  //https://medium.com/analytics-vidhya/create-github-actions-and-be-smart-f1e6b9cc9bfa
+
+
+
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+
 }
 
-
+main()
 
 /***/ }),
 
